85969853e27333564f11b9e63dee0ab4bac2cac0
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-11-04 17:15:17 +0000

    Created scores template

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  In class arrays
+// Purpose:     Allow the user the ability to enter homework scores from the 
+//              keyboard. There can be at most 20 scores and you are to enter
+//              the scores in an array scores. The program then calculates
+//              and displays the average after dropping the lowest score.
+//              Your program interaction is to be:
+//
+//              How many scores would you like: XX
+//              Enter Score 1: XXX.XX         and so on
+//
+//              Lowest Score Is: XXX.XX
+//              Average After Dropping Lowest Score Is: XXX.XX
+//  
+// Hours:       
 //******************************************************************************
 
 #include <iostream>
 
 using namespace std;
 
-int main () {
+void inputScores (double scores[], int numScores);
+double getTotal (const double scores[], int numScores);
+double getLowest (const double scores[], int numScores);
 
-  cout << "Hello World" << endl;
-  
+int main () {
+  const int MAX_SCORES = 20;
+
   return EXIT_SUCCESS;
 }
